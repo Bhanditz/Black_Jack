8234464c2f9f3d0b838d93a52890f01a64642719
Mostafa Anwar <39226625+MostafAanwar@users.noreply.github.com>
2018-09-06 05:53:23 +0200

    Add files via upload
    New version of the code where you get randomized shape from the four shapes of cards ♥ ♦ ♣ ♠

--- main.cpp
+++ main.cpp
@@ -1,21 +1,143 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <string>
 using namespace std;
 int generaterandnumber()
 {
-    //function to create a random number between
+    //A function to create a random number between 1 and 10 as...
+    //score of the cards
     return rand() % 10 + 1;
+}
+int generaterandnumber2()
+{
+    //A function to create a random number between 1 and 4
+    //to get random card shape ♥ ♦ ♣ ♠
+    return rand() % 4 + 1;
+}
+string cardChecker(int pc1,int pc2)
+{
+  //♥ ♦ ♣ ♠
+   switch( pc1)
+  {
+     case 1:
+  {
+    if(pc2==1)
+    {
+      return"♥";
+    }
+    else if(pc2==2)
+    {
+      return"♦";
+      
+    }
+    else if(pc2==3)
+    {
+      return"♣";
+      
+    }
+    else if(pc2==4)
+    { 
+      return"♠";      
+    }
+    break;
+  }
+  //♥ ♦ ♣ ♠
+  case 2:
+  { 
+    if(pc2==1)
+    {
+      return"♥";
+      
+    }
+    else if(pc2==2)
+    {
+      return"♦";
+      
+    }
+    else if(pc2==3)
+    {
+      return"♣";
+   
+    }
+    else if(pc2==4)
+    { 
+      return"♠";
+      
+    }
+    break;
+  }
+  //♥ ♦ ♣ ♠
+  case 3:
+  {
+
+    if(pc2==1)
+    {
+      return"♥";
+    }
+    else if(pc2==2)
+    {
+      return"♦";
+    }
+    else if(pc2==3)
+    {
+      return"♣";
+      
+    }
+    else if(pc2==4)
+    { 
+      return"♠";
+      
+    }
+    break;
+  }
+  //♥ ♦ ♣ ♠
+  case 4:
+  {
+    if(pc2==1)
+    {
+      return"♥";
+    
+    }
+    else if(pc2==2)
+    {
+      return"♦";
+      
+    }
+    else if(pc2==3)
+    {
+      return "♣";
+      
+    }
+    else if(pc2==4)
+    { 
+      return"♠";
+      
+    }
+    break;
+  }
+  }
 }
 int main()
 {
+/*this is a blackjack(aka 21) simple version your goal is to get 21 or anything closer to it but higher than the dealer's hand
+you can either hit(ask for another card) or stand(dont ask for another card)
+if you stand with x score if x is bigger than dealer score you win otherwise you lose
+if you get 21 automatically you will win and also if the dealer got 21 he wins*/
   int P_Card1,D_Card1,P_Card2,D_Card2;
   int P_Total=0;
   int D_Total=0;
+  //variables to store the number of the shape ...
+  // of the card
+  int pc1,pc2,dc1,dc2;
+  //variables to store the shape of the card
+  //♥ ♦ ♣ ♠
+  string PC1="",PC2="",DC1="";
   char playing='N',choice;
     srand ( time(NULL) );
     while(playing=='N')
     {
+      cout<<"_________♥ ♦ ♣ ♠ WELCOME TO BLACKJACK ♥ ♦ ♣ ♠_________"<<endl;
         //P_Card1 & 2 are player first 2 cards and D_Card1 & 2 are dealer first 2 cards
         P_Card1=generaterandnumber();
         P_Card2=generaterandnumber();
@@ -24,7 +146,15 @@ int main()
         //2 total variables to keep track of score in player and dealer hand
         P_Total=P_Card1+P_Card2;
         D_Total=D_Card1+D_Card2;
-        cout<<"Your first card is "<<P_Card1 <<" and second card is "<<P_Card2<<" with a total of "<<P_Total<<endl;
+        pc1=generaterandnumber2();
+        pc2=generaterandnumber2();
+        dc1=generaterandnumber2();
+        dc2=generaterandnumber2();
+        PC1=cardChecker(pc1,pc2);
+        PC2=cardChecker(pc1,pc2);
+        DC1=cardChecker(dc1,dc2);
+        cout<<"Your first card  is "<<P_Card1 <<" "<<PC1<<" and second card is "<<P_Card2<<" "<<PC2<<" with a total of "<<P_Total<<endl;
+        cout<<"One of the dealer's cards is  "<<D_Card1<<" "<<DC1<<endl;
         if(P_Total>21)
           cout<<"You are bust!!";
         if(P_Total==20 && (P_Card1==1 || P_Card2==1))
@@ -57,7 +187,7 @@ int main()
                     else if(P_Total==21)
                     {
                         cout<<"YOU WON!!"<<endl<<"Your total is: "<<P_Total<<", while Dealer's total is: "<<D_Total<<endl;
-                        break;
+                      break;
                     }
 
                     else
@@ -92,9 +222,9 @@ int main()
         }
         cout<<"Play again? (Y/N)"<<endl;
         cin>>choice;
-            if(choice=='N')
+            if(choice=='N'||choice=='n')
                break;
-            else if(choice=='Y')
+            else if(choice=='Y'||choice=='y')
                continue;
     }
 }
